20a87c175550a5a4ffcf853fa3c0ffc034fdead1
Saurabh81770 <techcroft.info@gmail.com>
2025-10-25 22:21:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,8 +7,22 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# VPS --- FILL COOKIES 🍪 in """ ... """ 
+# Pehle environment variables read karein
+API_ID = int(os.getenv("API_ID", ""))
+API_HASH = os.getenv("API_HASH", "")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "")
+OWNER_ID = list(map(int, os.getenv("OWNER_ID", "").split()))
+MONGO_DB = os.getenv("MONGO_DB", "")
+LOG_GROUP = os.getenv("LOG_GROUP", "")
+CHANNEL_ID = int(os.getenv("CHANNEL_ID", ""))
+FREEMIUM_LIMIT = int(os.getenv("FREEMIUM_LIMIT", "0"))
+PREMIUM_LIMIT = int(os.getenv("PREMIUM_LIMIT", "0"))
+WEBSITE_URL = os.getenv("WEBSITE_URL", "upshrink.com")
+AD_API = os.getenv("AD_API", "52b4a2cf4687d81e7d3f8f2b7bc2943f618e78cb")
+STRING = os.getenv("STRING", None)
+DEFAULT_SESSION = os.getenv("DEFAUL_SESSION", None)  # added old method of invite link joining
 
+# Phir default values define karein
 INST_COOKIES = """
 # wtite up here insta cookies
 """
@@ -17,19 +31,6 @@ YTUB_COOKIES = """
 # write here yt cookies
 """
 
-API_ID = int(getenv("API_ID", ""))
-API_HASH = getenv("API_HASH", "")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-OWNER_ID = list(map(int, getenv("OWNER_ID", "").split()))
-MONGO_DB = getenv("MONGO_DB", "")
-LOG_GROUP = getenv("LOG_GROUP", "")
-CHANNEL_ID = int(getenv("CHANNEL_ID", ""))
-FREEMIUM_LIMIT = int(getenv("FREEMIUM_LIMIT", "0"))
-PREMIUM_LIMIT = int(getenv("PREMIUM_LIMIT", "0"))
-WEBSITE_URL = getenv("WEBSITE_URL", "upshrink.com")
-AD_API = getenv("AD_API", "52b4a2cf4687d81e7d3f8f2b7bc2943f618e78cb")
-STRING = getenv("STRING", None)
-YT_COOKIES = getenv("YT_COOKIES", YTUB_COOKIES)
-DEFAULT_SESSION = getenv("DEFAUL_SESSION", None)  # added old method of invite link joining
-INSTA_COOKIES = getenv("INSTA_COOKIES", INST_COOKIES)
-
+# Ab in default values ko use kar sakte hain
+YT_COOKIES = os.getenv("YT_COOKIES", YTUB_COOKIES)
+INSTA_COOKIES = os.getenv("INSTA_COOKIES", INST_COOKIES)
