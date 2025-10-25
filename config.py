08fe73686c722bb1aee655d419cab3191d21a989
Saurabh81770 <techcroft.info@gmail.com>
2025-10-25 21:32:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -17,19 +17,19 @@ YTUB_COOKIES = """
 # write here yt cookies
 """
 
-API_ID = os.getenv("API_ID", "25292226")
-API_HASH = os.getenv("API_HASH", "a7d366626f54ca13916a01bd4ef121ab")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "7878942070:AAHWQ4VjCd9DtTI3Q4W-zLLlde4i1_SzqYE")
-MONGO_DB = os.getenv("MONGO_DB", "mongodb+srv://vafabol902:5yVC8KLFmWdTLEfy@cluster0.ghld1c1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-OWNER_ID = list(map(int, os.getenv("OWNER_ID", "7445035418").split())) # list seperated via space
-DB_NAME = os.getenv("DB_NAME", "telegram_downloader")
-STRING = os.getenv("STRING", None) # optional
-LOG_GROUP = int(os.getenv("LOG_GROUP", "-1002951355718")) # optional with -100
-FORCE_SUB = int(os.getenv("FORCE_SUB", "-1002525108492")) # optional with -100
-MASTER_KEY = os.getenv("MASTER_KEY", "gK8HzLfT9QpViJcYeB5wRa3DmN7P2xUq") # for session encryption
-IV_KEY = os.getenv("IV_KEY", "s7Yx5CpVmE3F") # for decryption
-YT_COOKIES = os.getenv("YT_COOKIES", YTUB_COOKIES)
-INSTA_COOKIES = os.getenv("INSTA_COOKIES", INST_COOKIES)
-FREEMIUM_LIMIT = int(os.getenv("FREEMIUM_LIMIT", "0"))
-PREMIUM_LIMIT = int(os.getenv("PREMIUM_LIMIT", "500"))
+API_ID = int(getenv("API_ID", ""))
+API_HASH = getenv("API_HASH", "")
+BOT_TOKEN = getenv("BOT_TOKEN", "")
+OWNER_ID = list(map(int, getenv("OWNER_ID", "").split()))
+MONGO_DB = getenv("MONGO_DB", "")
+LOG_GROUP = getenv("LOG_GROUP", "")
+CHANNEL_ID = int(getenv("CHANNEL_ID", ""))
+FREEMIUM_LIMIT = int(getenv("FREEMIUM_LIMIT", "0"))
+PREMIUM_LIMIT = int(getenv("PREMIUM_LIMIT", "0"))
+WEBSITE_URL = getenv("WEBSITE_URL", "upshrink.com")
+AD_API = getenv("AD_API", "52b4a2cf4687d81e7d3f8f2b7bc2943f618e78cb")
+STRING = getenv("STRING", None)
+YT_COOKIES = getenv("YT_COOKIES", YTUB_COOKIES)
+DEFAULT_SESSION = getenv("DEFAUL_SESSION", None)  # added old method of invite link joining
+INSTA_COOKIES = getenv("INSTA_COOKIES", INST_COOKIES)
 
